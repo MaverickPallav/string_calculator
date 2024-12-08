309049c38bb7ecb7444ce296ab821e7849177a37
pallav.sharma <pallavps17@gmail.com>
2024-12-08 17:18:02 +0530

    multi number add function

--- calculator_test.py
+++ calculator_test.py
@@ -2,17 +2,20 @@ import unittest
 from calculator import StringCalculator
 
 class StringCalculatorTest(unittest.TestCase):
+    def setUp(self):
+        self.calculator = StringCalculator()
+
     def test_empty_string(self):
-        calculator = StringCalculator()
-        self.assertEqual(calculator.add(""), 0)
+        self.assertEqual(self.calculator.add(""), 0)
     
     def test_one_number_string(self):
-        calculator = StringCalculator()
-        self.assertEqual(calculator.add("1"), 1)
+        self.assertEqual(self.calculator.add("1"), 1)
     
     def test_two_number_string(self):
-        calculator = StringCalculator()
-        self.assertEqual(calculator.add("1, 2"), 3)
+        self.assertEqual(self.calculator.add("1, 2"), 3)
+
+    def test_multiple_number_string(self):
+        self.assertEqual(self.calculator.add("1, 2, 3, 4, 5"), 15)
 
 if __name__ == '__main__':
     unittest.main()
